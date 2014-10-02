4915346de477411478373011a508444ea41142de
Tongqing Qiu <tqiu@liaison.com>
2014-10-02 15:20:04 -0400

    Initial commit

---
+++ src/Test.groovy
@@ -0,0 +1,14 @@
+use(DistanceCategory.class) {
+    def d1 = 1.m
+    def d2 = 1.yd
+    def d3 = 1760.yd
+    def d4 = 100.cm
+    println d1 + 1.yd
+    println 1.yd + 1.mi
+    println 1.m - 1.yd
+    println d2.m
+    println d3.mi
+    println d4.m
+    println 1000.yd.km
+    println 1000.yd
+}
